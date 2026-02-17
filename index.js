6c0543ee8bd6014ae33f81d36c3fb992beed4b5c
RahulBaldia <rahulbaldia2280@gmail.com>
2026-02-17 12:02:13 +0530

    Other Commit

--- index.js
+++ index.js
@@ -10,9 +10,4 @@ app.get("/home", (req, res) => {
   res.send("Welcome to My Home");
 });
 
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
-    
+app.listen(3000)
