f2eb6ee081f66402088dac1bbeeaf450c303dfe6
RahulBaldia <rahulbaldia2280@gmail.com>
2026-02-17 11:28:35 +0530

    Updated port handling

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require("express");
 
 const app = express();
 
-app.get("/home", (req, res) => {
+app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(3000);
+app.listen(4000);
