f7d23064d93952afaade8cdd5eaabc7705a52406
RahulBaldia <rahulbaldia2280@gmail.com>
2026-02-21 15:56:42 +0530

    Dbcommit

--- DatabaseLearn/src/app.js
+++ DatabaseLearn/src/app.js
@@ -1,1 +1,22 @@
 // Server ko create and config karna 
+
+const express = require("express")
+const noteModel = require('./models/notes.model.js')
+
+const app = express();
+app.use(express.json())
+
+
+app.post('/notes', async (req,res) =>{
+    const{title,description} = req.body
+    const note = await noteModel.create({
+        title, description
+    })
+
+    res.status(201).json({
+        message:"Note Created Successfully",
+        note
+    })
+})
+
+module.exports = app
--- DatabaseLearn/src/server.js
+++
@@ -1,1 +0,0 @@
-// Server ko start karna 
