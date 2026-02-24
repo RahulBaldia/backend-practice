b5f61bb6c0d286cc4ab506387ac86e214a7fcab9
RahulBaldia <rahulbaldia2280@gmail.com>
2026-02-24 17:43:34 +0530

    Updated backend fixes 2

--- FullStack/Backend/src/app.js
+++ FullStack/Backend/src/app.js
@@ -59,8 +59,8 @@ app.patch("/notes/:id", async (req, res) => {
     });
 });
 
-app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/index.html"));
+app.get("*name", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "/public/index.html"));
 });
 
 module.exports = app
